8f0d4c2c8fae8741a100dca7c8a3a44490e87e92
jwvhewitt <jwvhewitt@e07c548f-c52c-0410-b29b-ad8007db0e57>
2007-10-01 14:42:28 +0000

    git-svn-id: https://gearhead2.svn.sourceforge.net/svnroot/gearhead2@160 e07c548f-c52c-0410-b29b-ad8007db0e57

--- trunk/arenacfe.pp
+++ trunk/arenacfe.pp
@@ -856,7 +856,7 @@ Function MightSurrender( GB: GameBoardPtr; NPC: GearPtr ): Boolean;
 	{ Return TRUE if it's possible that NPC might surrender given its }
 	{ current situation, or FALSE otherwise. }
 begin
-	MightSurrender := ( CurrentStamina(NPC) = 0 ) and AreEnemies(GB,NAttValue(NPC^.NA,NAG_Location,NAS_Team),NAV_DefPlayerTeam) and (NAttValue(NPC^.NA,NAG_EpisodeData,NAS_SurrenderStatus) = 0) and NotAnAnimal( NPC );
+	MightSurrender := (( CurrentStamina(NPC) = 0 ) or ( GearCurrentDamage( NPC ) < Random( 4 ) )) and AreEnemies(GB,NAttValue(NPC^.NA,NAG_Location,NAS_Team),NAV_DefPlayerTeam) and (NAttValue(NPC^.NA,NAG_EpisodeData,NAS_SurrenderStatus) = 0) and NotAnAnimal( NPC );
 end;
 
 Procedure VerbalAttack( GB: GameBoardPtr; Attacker,Target: GearPtr );
