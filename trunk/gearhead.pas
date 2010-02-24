5eba74921f823c10abcdf35287d57715d91b5a34
jwvhewitt <jwvhewitt@e07c548f-c52c-0410-b29b-ad8007db0e57>
2010-02-24 08:44:28 +0000

    release for v0.622
    git-svn-id: https://gearhead2.svn.sourceforge.net/svnroot/gearhead2@616 e07c548f-c52c-0410-b29b-ad8007db0e57

--- trunk/gearhead.pas
+++ trunk/gearhead.pas
@@ -43,7 +43,7 @@ uses 	gears,navigate,randmaps,locale,arenaplay,ghchars,gearutil,gearparser,
 {$ENDIF}
 
 const
-	Version = '0.621';
+	Version = '0.622';
 
 Procedure RedrawOpening;
 	{ The opening menu redraw procedure. }
