56454085ee286e6a75193861358fe8725536b676
jwvhewitt <jwvhewitt@e07c548f-c52c-0410-b29b-ad8007db0e57>
2010-04-23 14:17:08 +0000

    Changing mekcheck to print army lists
    git-svn-id: https://gearhead2.svn.sourceforge.net/svnroot/gearhead2@650 e07c548f-c52c-0410-b29b-ad8007db0e57

--- trunk/mekcheck.pas
+++ trunk/mekcheck.pas
@@ -19,10 +19,47 @@ begin
 	end;
 end;
 
-Procedure Examine_Mecha_List( mecha_list: GearPtr; Facs: String );
+Procedure Examine_Mecha_List( mecha_list: GearPtr; Facs,Faction_Desig: String );
 	{ Examine the mecha list. See how complete its mecha spectrum is. }
+const
+	UTYPE_General = 'GENERAL';
+	UTYPE_Assault = 'ASSAULT';
+	UTYPE_Defense = 'DEFENSE';
+	UType_Desig: Array [1..3] of String = (
+		UTYPE_General, UTYPE_Assault, UTYPE_Defense
+	);
+	ROLE_Trooper = 1;
+	ROLE_Support = 2;
+	ROLE_Command = 3;
+
+	Function MechaRole( Mek: GearPtr; const UNIT_TYPE: String ): Integer;
+		{ Return the role of this mecha, given the faction and unit type. }
+	var
+		roles: String;
+		P: Integer;
+	begin
+		{ Error check- if no faction, no role. }
+		if Faction_Desig = '' then Exit( 0 );
+
+		{ Obtain the proper ROLE string from the mecha. }
+		roles := UpCase( SAttValue( Mek^.SA , 'ROLE_' + Faction_Desig ) );
+
+		{ Next, locate the clause pertaining to this unit type. }
+		P := Pos( Unit_Type , roles );
+		if ( P <> 0 ) and ( Length( roles ) >= ( P + Length( Unit_Type ) + 2 ) ) then begin
+			Case roles[ P + Length( Unit_Type ) + 1 ] of
+				'T':	P := ROLE_Trooper;
+				'S':	P := ROLE_Support;
+				'C':	P := ROLE_Command;
+			else P := 0;
+			end;
+		end;
+		MechaRole := P;
+	end;
+
 var
 	Mecha_Graph: Array [1..10,1..3] of Integer;
+	Army_List: Array [1..3,1..3] of SAttPtr;	{ X = Unit Type, Y = Role }
 	mek: GearPtr;
 	t,tt,total: Integer;
 	mekval: LongInt;
@@ -31,6 +68,13 @@ begin
 	for t := 1 to 10 do begin
 		for tt := 1 to 3 do begin
 			Mecha_Graph[ t , tt ] := 0;
+		end;
+	end;
+
+	{ Also clear the army list. }
+	for t := 1 to 3 do begin
+		for tt := 1 to 3 do begin
+			Army_List[ t , tt ] := Nil;
 		end;
 	end;
 
@@ -53,6 +97,15 @@ begin
 				Inc( Mecha_Graph[ tt , 1 ] );
 				Inc( Total );
 			end;
+
+			{ While we're at it, file this mek away in the army }
+			{ list, if appropriate. }
+			for t := 1 to 3 do begin
+				tt := MechaRole( Mek , UTYPE_Desig[ t ] );
+				if ( tt >= 1 ) and ( tt <= 3 ) then begin
+
+				end;
+			end;
 		end;
 
 		mek := mek^.next;
@@ -65,6 +118,13 @@ begin
 		writeln();
 	end;
 	writeln( ' Total: ' , total );
+
+	{ Dispose of the army list. }
+	for t := 1 to 3 do begin
+		for tt := 1 to 3 do begin
+			DisposeSAtt( Army_List[ t , tt ] );
+		end;
+	end;
 end;
 
 Procedure CalcBARat( llist: GearPtr );
